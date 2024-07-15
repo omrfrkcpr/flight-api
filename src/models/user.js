6ea99e572440c94988e2bd75060e389eb688c3e7
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-15 09:42:54 +0200

    chore: Update user model with avatar, isStaff, and isStaff fields

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,8 @@
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
 const passwordEncrypt = require("../helpers/passwordEncrypt");
+const { CustomError } = require("../errors/customError");
+
 /* ------------------------------------------------------- */
 
 const UserSchema = new mongoose.Schema(
@@ -18,7 +20,7 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      set: (password) => passwordEncrypt(password),
+      // set: (password) => passwordEncrypt(password),
     },
     email: {
       type: String,
@@ -30,9 +32,17 @@ const UserSchema = new mongoose.Schema(
         "Email type is not correct.",
       ],
     },
+    avatar: {
+      type: String,
+      trim: true,
+    },
     isActive: {
       type: Boolean,
       default: true,
+    },
+    isStaff: {
+      type: Boolean,
+      default: false,
     },
     isAdmin: {
       type: Boolean,
@@ -44,5 +54,47 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Just for create operation
+UserSchema.pre("validate", function (next) {
+  console.log(this.password);
+  if (
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-\*?+&%{}@])[A-Za-z\d!-\*?+&%{}@]{8,}$/.test(
+      this.password
+    )
+  ) {
+    next();
+  } else {
+    throw new CustomError("Password type is incorrect!", 400);
+  }
+});
+
+UserSchema.pre("save", function (next) {
+  this.password = passwordEncrypt(this.password);
+
+  // if (this.isModified("password")) {
+  //   this.password = passwordEncrypt(this.password);
+  // }
+  next();
+});
+
+const updateEncryptValidatePassword = function (next) {
+  const update = this.getUpdate();
+  if (update.password) {
+    if (
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-\*?+&%{}@])[A-Za-z\d!-\*?+&%{}@]{8,}$/.test(
+        update.password
+      )
+    ) {
+      update.password = passwordEncrypt(update.password);
+    }
+  } else {
+    throw new CustomError("Password type is incorrect!", 400);
+  }
+  // this.update({}, { $set: { password: passwordEncrypt(this.getUpdate().$set.password) } });
+  next();
+};
+
+UserSchema.pre("updateOne", updateEncryptValidatePassword);
 
 module.exports = mongoose.model("User", UserSchema);
